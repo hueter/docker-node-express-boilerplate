e2be0b48380493bee3f3e677ad86f2abc994ea24
Michael Hueter <mthueter@gmail.com>
2020-05-22 10:50:18 -0700

    address mongoose deprecation warnings

--- app/config.js
+++ app/config.js
@@ -1,19 +1,19 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const APP_NAME = 'Boilerplate API';
+const APP_NAME = "Boilerplate API";
 const ENV = process.env.NODE_ENV;
 const PORT = process.env.PORT || 5000;
 
 // database configs
 
-let MONGODB_URI = process.env.MONGODB_URI || 'mongodb://mongodb/boilerplate';
-if (ENV === 'test') {
+let MONGODB_URI = process.env.MONGODB_URI || "mongodb://mongodb/boilerplate";
+if (ENV === "test") {
   MONGODB_URI = global.__MONGO_URI__;
 }
 
 mongoose.Promise = Promise;
-if (ENV === 'development' || ENV === 'test') {
-  mongoose.set('debug', true);
+if (ENV === "development" || ENV === "test") {
+  mongoose.set("debug", true);
 }
 
 /**
@@ -21,10 +21,12 @@ if (ENV === 'development' || ENV === 'test') {
  */
 async function connectToDatabase() {
   try {
-    await mongoose.connect(
-      MONGODB_URI,
-      { autoIndex: false, useNewUrlParser: true }
-    );
+    await mongoose.connect(MONGODB_URI, {
+      autoIndex: false,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false
+    });
     console.log(`${APP_NAME} successfully connected to database.`);
   } catch (error) {
     console.log(error);
@@ -37,16 +39,16 @@ async function connectToDatabase() {
  *  You can also just use the 'cors' package.
  */
 function globalResponseHeaders(request, response, next) {
-  response.header('Access-Control-Allow-Origin', '*');
+  response.header("Access-Control-Allow-Origin", "*");
   response.header(
-    'Access-Control-Allow-Headers',
-    'Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization'
+    "Access-Control-Allow-Headers",
+    "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization"
   );
   response.header(
-    'Access-Control-Allow-Methods',
-    'POST,GET,PATCH,DELETE,OPTIONS'
+    "Access-Control-Allow-Methods",
+    "POST,GET,PATCH,DELETE,OPTIONS"
   );
-  response.header('Content-Type', 'application/json');
+  response.header("Content-Type", "application/json");
   return next();
 }
 
