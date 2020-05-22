ee9a58dc9437967b7ffe5d2123f60e2f3c87486c
Michael Hueter <mthueter@gmail.com>
2020-05-22 11:08:39 -0700

    more prettier

--- config/jest/setup.js
+++ config/jest/setup.js
@@ -1,17 +1,17 @@
 // setup.js
-const path = require('path');
+const path = require("path");
 
-const fs = require('fs');
+const fs = require("fs");
 
-const MongodbMemoryServer = require('mongodb-memory-server');
+const MongodbMemoryServer = require("mongodb-memory-server");
 
-const globalConfigPath = path.join(__dirname, 'globalConfig.json');
+const globalConfigPath = path.join(__dirname, "globalConfig.json");
 
 const mongoServer = new MongodbMemoryServer.MongoMemoryServer();
 
 module.exports = async function() {
   const mongoConfig = {
-    mongoDBName: 'jest',
+    mongoDBName: "jest",
     mongoUri: await mongoServer.getConnectionString()
   };
 
--- config/jest/teardown.js
+++ config/jest/teardown.js
@@ -1,4 +1,4 @@
 module.exports = async function() {
-  console.log('Teardown mongod server.');
+  console.log("Teardown mongod server.");
   await global.__MONGOD__.stop();
 };
